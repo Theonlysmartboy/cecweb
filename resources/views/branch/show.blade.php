49dee5066b5a8219938541597d1ad002d27ee3f9
Tosby <o2jose43@gmail.com>
2020-06-05 13:00:18 +0300

    House keeping

--- resources/views/branch/show.blade.php
+++ resources/views/branch/show.blade.php
@@ -15,7 +15,7 @@
 <!--//banner_info-->
 <!--/ab-->
 <div class="banner_bottom">
-    <div class="container">
+    <div class="container jumbotron">
       <h3 class="tittle-w3ls">{{$branch_details->name}}</h3>
       <div class="inner_sec_info_wthree_agile">
         <div class="help_full">
@@ -26,13 +26,34 @@
             <h4>About</h4>
             <p>{{$branch_details->description}} </p>
                 <h4>Our Vision</h4>
-                <p>To see people evangelized, discipled, equipped, empowered and serving God faithfully.</p>
+                <p>{{$branch_details->vision}}.</p>
                 <h4>Mission Statement</h4>
-                <p>To win people to Jesus Christ, disciple them to disciple nations so that they can affect the churches and communities world over.</p>
+                <p>{{$branch_details->mission}}.</p>
           </div>
         </div>
       </div>
     </div>
   </div>
 <!--//ab-->
+<div class="banner_bottom">
+    <div class="container jumbotron">
+      <h3 class="tittle-w3ls">Head Pastor</h3>
+      <div class="inner_sec_info_wthree_agile">
+        <div class="help_full">
+            <div class="col-md-6 banner_bottom_left">
+                <h4>Name</h4>
+                <p>{{$head_pastor->title ."".$head_pastor->name}} </p>
+                    <h4>Message</h4>
+                    <p>{{$branch_details->pst_message}}.</p>
+                    <h4>Biography</h4>
+                    <p>{{$head_pastor->bio}}.</p>
+              </div>
+          <div class="col-md-6 banner_bottom_grid help">
+            <img src="{{ asset('images/webimgs/banner1.jpg') }}" alt=" " class="img-responsive">
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
